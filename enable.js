f385234973d981e6597307101c5e09ebcbe132f6
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-10-01 14:13:04 +0300

    Create enable.js

---
+++ enable.js
@@ -0,0 +1,10 @@
+(function () {
+    'use strict';
+
+    Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/bookmarks.js'], function () {});
+    Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/interfaceColor.js'], function () {});
+    Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/interfaceSize.js'], function () {});
+    Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/interfaceStyle.js'], function () {});
+    Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/quality.js'], function () {});
+
+})();
