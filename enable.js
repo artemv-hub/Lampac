ae6b7eeae577b4d2e96150594499d2cadee70b69
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-10-07 12:20:59 +0300

    1.1.1

--- enable.js
+++ enable.js
@@ -2,7 +2,7 @@
   "use strict";
   
   var manifest = {
-    version: "1.1.0",
+    version: "1.1.1",
     name: "artemv_plugins",
     description: "ArtemV Plugins",
   };
@@ -11,7 +11,7 @@
   if (Lampa.Storage.get('lampac_unic_id', '') === 'ArtemV') {
     Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/bookmarks.js'], function () {});
     Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/interfaceColor.js'], function () {});
-    Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/interfaceMobile.js'], function () {});
+    //Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/interfaceMobile.js'], function () {});
     Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/interfaceSize.js'], function () {});
     Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/interfaceStyle.js'], function () {});
     Lampa.Utils.putScriptAsync(['https://artemv-hub.github.io/Lampac/plugins/quality.js'], function () {});
