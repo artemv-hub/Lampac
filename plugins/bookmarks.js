61349610976c3bf8bbff5e74d37a69e5fd4a38ab
Артём <82865318+artemv-hub@users.noreply.github.com>
2025-11-03 23:58:07 +0300

    Update bookmarks.js

--- plugins/bookmarks.js
+++ plugins/bookmarks.js
@@ -1,84 +1,8 @@
 (function () {  
   "use strict";  
     
-  var LAMPAC_HOST = 'http://217.21.60.201:15366';  
-    
-  function CustomCategoryPlugin() {  
-    // Получить все закладки с сервера  
-    this.getBookmarks = function(callback) {  
-      fetch(LAMPAC_HOST + '/bookmark/list')  
-        .then(r => r.json())  
-        .then(callback)  
-        .catch(err => console.error('Failed to load bookmarks:', err));  
-    };  
-      
-    // Создать или обновить категорию  
-    this.setCategory = function(categoryName, cardIds, callback) {  
-      fetch(LAMPAC_HOST + '/bookmark/set', {  
-        method: 'POST',  
-        headers: { 'Content-Type': 'application/json' },  
-        body: JSON.stringify({  
-          where: categoryName,  
-          data: cardIds  
-        })  
-      })  
-      .then(r => r.json())  
-      .then(callback)  
-      .catch(err => console.error('Failed to set category:', err));  
-    };  
-      
-    // Добавить карточку в категорию  
-    this.addToCategory = function(categoryName, cardId) {  
-      var self = this;  
-      this.getBookmarks(function(bookmarks) {  
-        var currentIds = bookmarks[categoryName] || [];  
-          
-        if (currentIds.indexOf(cardId) === -1) {  
-          currentIds.unshift(cardId);  
-          self.setCategory(categoryName, currentIds, function() {  
-            Lampa.Noty.show('Добавлено в ' + categoryName);  
-          });  
-        }  
-      });  
-    };  
-      
-    // Удалить карточку из категории  
-    this.removeFromCategory = function(categoryName, cardId) {  
-      var self = this;  
-      this.getBookmarks(function(bookmarks) {  
-        var currentIds = bookmarks[categoryName] || [];  
-        var index = currentIds.indexOf(cardId);  
-          
-        if (index !== -1) {  
-          currentIds.splice(index, 1);  
-          self.setCategory(categoryName, currentIds, function() {  
-            Lampa.Noty.show('Удалено из ' + categoryName);  
-          });  
-        }  
-      });  
-    };  
-      
-    // Получить список пользовательских категорий  
-    this.getCustomCategories = function(callback) {  
-      this.getBookmarks(function(bookmarks) {  
-        var categories = [];  
-        var standardCategories = ['history', 'like', 'watch', 'wath', 'book', 'look', 'viewed', 'scheduled', 'continued', 'thrown', 'card'];  
-          
-        for (var key in bookmarks) {  
-          if (bookmarks.hasOwnProperty(key) && standardCategories.indexOf(key) === -1) {  
-            categories.push({  
-              name: key,  
-              count: Array.isArray(bookmarks[key]) ? bookmarks[key].length : 0  
-            });  
-          }  
-(function () {  
-  "use strict";  
-    
   var LAMPAC_HOST = window.location.origin;  
     
-  // ============================================  
-  // CORE: Управление категориями через API  
-  // ============================================  
   function CustomCategoryAPI() {  
     var self = this;  
       
@@ -137,7 +61,6 @@
         if (currentIds.indexOf(card.id) === -1) {  
           currentIds.unshift(card.id);  
             
-          // Добавить карточку в общий массив, если её там нет  
           var cardExists = cardArray.some(function(c) { return c.id === card.id; });  
           if (!cardExists) {  
             cardArray.unshift(card);  
@@ -178,9 +101,6 @@
     
   var api = new CustomCategoryAPI();  
     
-  // ============================================  
-  // UI: Страница закладок  
-  // ============================================  
   function BookmarksPageUI() {  
     this.renderCategoryButton = function(category) {  
       var $register = Lampa.Template.js('register')  
@@ -191,7 +111,6 @@
       $register.find('.register__name').text(category.name);  
       $register.find('.register__counter').text(category.count || 0);  
         
-      // Клик - открыть категорию  
       $register.on('hover:enter', function() {  
         Lampa.Activity.push({  
           url: '',  
@@ -202,7 +121,6 @@
         });  
       });  
         
-      // Долгое нажатие - меню управления  
       $register.on('hover:long', function() {  
         var menu = [  
           { title: 'Переименовать', action: 'rename' },  
@@ -226,7 +144,6 @@
                 nosave: true  
               }, function(newName) {  
                 if (newName && newName !== category.name) {  
-                  // Переименование = создание новой + удаление старой  
                   api.getBookmarks(function(bookmarks) {  
                     var ids = bookmarks[category.name] || [];  
                     api.setCategory(newName, ids, function() {  
@@ -265,7 +182,6 @@
           if (value) {  
             api.createCategory(value, function() {  
               Lampa.Noty.show('Категория "' + value + '" создана');  
-              // Перезагрузить страницу закладок  
               Lampa.Activity.active().activity.toggle();  
             });  
           }  
@@ -278,11 +194,9 @@
     this.render = function() {  
       var $render = Lampa.Activity.active().activity.render();  
         
-      // Добавить кнопку создания  
       var $addBtn = this.renderAddButton();  
       $('.register:first', $render).after($addBtn);  
         
-      // Добавить существующие категории  
       api.getCustomCategories(function(categories) {  
         categories.forEach(function(category) {  
           var $btn = new BookmarksPageUI().renderCategoryButton(category);  
@@ -294,9 +208,6 @@
     };  
   }  
     
-  // ============================================  
-  // UI: Контекстное меню карточки  
-  // ============================================  
   function CardMenuUI() {  
     this.extend = function(card) {  
       api.getCustomCategories(function(categories) {  
@@ -344,14 +255,10 @@
     };  
   }  
     
-  // ============================================  
-  // INIT: Инициализация плагина  
-  // ============================================  
   function init() {  
     if (window.customCategoriesPlugin) return;  
     window.customCategoriesPlugin = true;  
       
-    // Рендерить категории на странице закладок  
     Lampa.Listener.follow('activity', function(e) {  
       if (e.type === 'start' && e.component === 'bookmarks') {  
         setTimeout(function() {  
@@ -360,7 +267,6 @@
       }  
     });  
       
-    // Расширить меню карточки  
     Lampa.Listener.follow('full', function(e) {  
       if (e.type === 'complite') {  
         var $bookBtn = $('.button--book', Lampa.Activity.active().activity.render());  
@@ -375,7 +281,6 @@
     console.log('[CustomCategories] Plugin initialized');  
   }  
     
-  // Запуск  
   if (window.appready) {  
     init();  
   } else {  
